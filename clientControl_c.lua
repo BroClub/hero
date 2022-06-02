fc0b54edf0c7ed150c63d9a9b039351744af575c
mbronner <gabba_marco@hotmail.de>
2022-06-02 23:20:03 +0200

    Actual use for onCalculate

--- clientControl_c.lua
+++ clientControl_c.lua
@@ -1,3 +1,6 @@
+local directions = { [0] = 'N', [45] = 'NW', [90] = 'W', [135] = 'SW', [180] = 'S', [225] = 'SE', [270] = 'E', [315] = 'NE', [360] = 'N', } 
+local direction = 'N' -- default text, will be changed soon. 
+
 -- called once this resource starts. Since this is clientside, this resource start when the player connected
 function onStartup()
     print("onStartup")
@@ -14,25 +17,53 @@ function onStartup()
     SetEntityHealth(playerPed, 600)
 
     -- super kick power. or unarmed power. Normal is 1.5
-    SetWeaponDamageModifier('WEAPON_UNARMED', 950.9)
+    SetWeaponDamageModifier('WEAPON_UNARMED', 1000.0)
 
-    -- SetPedMoveRateOverride(PlayerId(),10.0)
-    SetRunSprintMultiplierForPlayer(PlayerId(), 2.49) -- normal is 1.0
+    SetRunSprintMultiplierForPlayer(PlayerId(), 1.49) -- normal is 1.0
 
     SetSwimMultiplierForPlayer(PlayerId(), 1.49) -- normal is 1.0
 end
 
 -- called on every frame. Used for ingame Settings. No calculations like distances
 function onFrame()
-    SetSuperJumpThisFrame(PlayerId(), 2000)
-    RestorePlayerStamina(PlayerId(), 100)
+    SetSuperJumpThisFrame(PlayerId(), 2000) -- Native, needs to be called every Frame
+    RestorePlayerStamina(PlayerId(), 100) -- Native, resets player stamina, therefore makes it basically infinite 
+
+    drawTxt(
+        0.685, -- x position. 
+        1.42,  -- y position
+        1.0, -- width
+        1.0,-- height
+        1.0,  --scale
+        direction, -- actual value calculated NOT every Frame. 
+        255, 255, 255, 1000
+    )
 end
 
 -- called every 500ms. Used for expensive calculations.
 function onCalculation()
+    for k,v in pairs(directions)do
+        direction = GetEntityHeading(PlayerPedId()) -- where is the player looking at
+        if(math.abs(direction - k) < 22.5)then -- heavy calculation. math.abs is really expensive. 
+            direction = v
+            break
+        end
+    end
 end
 
-
+function drawTxt(x,y ,width,height,scale, text, r,g,b,a)
+    SetTextFont(6)
+    SetTextProportional(0)
+    SetTextScale(scale, scale)
+    SetTextColour(r, g, b, a)
+    SetTextDropShadow(0, 0, 0, 0,255)
+    SetTextEdge(1, 0, 0, 0, 255)
+    SetTextDropShadow()
+    SetTextOutline()
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(x - width/2, y - height/2 + 0.005)
+end
 
 -- start every frame function
 AddEventHandler("onClientResourceStart", function (resourcename)
